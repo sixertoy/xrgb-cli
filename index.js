10cd582ca22e58b127c7ce95b19bdc5dec498e28
sixertoy <sixertoy.github@gmail.com>
2018-01-29 16:37:58 +0100

    add random color if none args

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const isvalidrgb = require('./lib/isvalid-rgb');
 const isvalidhex = require('./lib/isvalid-hex');
 
 const args = process.argv.slice(2);
+
+if (!args || !args.length) {
+  // random color
+  const colors = Array.from({ length: 3 }, () => Math.floor(Math.random() * 255)).join(',');
+  tohex(['rgb', colors]);
+  process.exit(0);
+}
+
 const ishex = isvalidhex(args[0]);
 const isrgb = isvalidrgb(args[0]);
 
